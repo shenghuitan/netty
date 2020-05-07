f1959b5bfcf62f6b7a7ae34d3276d7c659744336
tanshenghui1 <tanshenghui1@yy.com>
2020-05-07 12:39:22 +0800

    EventExecutorGroup注释

--- common/src/main/java/io/netty/util/concurrent/EventExecutorGroup.java
+++ common/src/main/java/io/netty/util/concurrent/EventExecutorGroup.java
@@ -26,17 +26,28 @@ import java.util.concurrent.TimeUnit;
  * via its {@link #next()} method. Besides this, it is also responsible for handling their
  * life-cycle and allows shutting them down in a global fashion.
  *
+ * EventExecutorGroup是一个负责的，通过next方法提供可使用的EventExecutor。
+ * 除此之外，它也是一个负责的，管理它们的生命周期，且允许通过一个全局的方式来关闭它们。
+ *
+ * NOTE
+ *
+ * 实际上，EventExecutorGroup就是个一个管理容器，专门用于管理EventExecutor，类比连接池，线程池等，并没有太大的不同。
+ *
  */
 public interface EventExecutorGroup extends ScheduledExecutorService, Iterable<EventExecutor> {
 
     /**
      * Returns {@code true} if and only if all {@link EventExecutor}s managed by this {@link EventExecutorGroup}
      * are being {@linkplain #shutdownGracefully() shut down gracefully} or was {@linkplain #isShutdown() shut down}.
+     *
+     * 返回true，当且仅当所有由当前EventExecutorGroup管理的EventExecutors被shutdownGracefully正在被优雅关闭，或已被关闭。
      */
     boolean isShuttingDown();
 
     /**
      * Shortcut method for {@link #shutdownGracefully(long, long, TimeUnit)} with sensible default values.
+     *
+     * shutdownGracefully的快捷方法，使用合理的默认参数关闭。
      *
      * @return the {@link #terminationFuture()}
      */
@@ -49,10 +60,18 @@ public interface EventExecutorGroup extends ScheduledExecutorService, Iterable<E
      * (usually a couple seconds) before it shuts itself down.  If a task is submitted during the quiet period,
      * it is guaranteed to be accepted and the quiet period will start over.
      *
+     * 信号通知当前执行器executor，调用方希望这个executor被关闭。一旦这个方法被调用，isShuttingDown将返回true，
+     * 并且executor准备自行关闭。
+     * 与shutdown方法不一样，优雅关闭（graceful shutdown）保证了没有任务会被提交在静默期间（通常几秒），在它自行关闭前。
+     * 如果一个任务在静默期间被提交了，它将保证任务被接收执行，并且静默周期重新开始。
+     *
      * @param quietPeriod the quiet period as described in the documentation
+     *                    文档中描述的安静周期
      * @param timeout     the maximum amount of time to wait until the executor is {@linkplain #shutdown()}
      *                    regardless if a task was submitted during the quiet period
+     *                    最长等待时间，直到执行器executor关闭，不管一个任务是否已经提交了，在安静周期期间
      * @param unit        the unit of {@code quietPeriod} and {@code timeout}
+     *                    时间单位
      *
      * @return the {@link #terminationFuture()}
      */
@@ -61,11 +80,15 @@ public interface EventExecutorGroup extends ScheduledExecutorService, Iterable<E
     /**
      * Returns the {@link Future} which is notified when all {@link EventExecutor}s managed by this
      * {@link EventExecutorGroup} have been terminated.
+     *
+     * 当被这个EventExecutorGroup管理的所有EventExecutor被关闭的时候，返回一个Future通知。
      */
     Future<?> terminationFuture();
 
     /**
      * @deprecated {@link #shutdownGracefully(long, long, TimeUnit)} or {@link #shutdownGracefully()} instead.
+     *
+     * 过期方法，将使用shutdownGracefully代替。
      */
     @Override
     @Deprecated
@@ -73,6 +96,8 @@ public interface EventExecutorGroup extends ScheduledExecutorService, Iterable<E
 
     /**
      * @deprecated {@link #shutdownGracefully(long, long, TimeUnit)} or {@link #shutdownGracefully()} instead.
+     *
+     * 过期方法，将使用shutdownGracefully代替。
      */
     @Override
     @Deprecated
@@ -80,6 +105,9 @@ public interface EventExecutorGroup extends ScheduledExecutorService, Iterable<E
 
     /**
      * Returns one of the {@link EventExecutor}s managed by this {@link EventExecutorGroup}.
+     *
+     * 返回下一个被当前EventExecutorGroup管理的EventExecutor。
+     *
      */
     EventExecutor next();
 
