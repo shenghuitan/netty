3f402d81de5e1b5bab9785af7a84b123013cfb11
tanshenghui1 <tanshenghui1@yy.com>
2020-08-17 00:39:41 +0800

    理解Invoker，主要在于上下文的访问和管理，可以看到，这是一个典型的next链式结构。

--- transport/src/main/java/io/netty/channel/ChannelInboundInvoker.java
+++ transport/src/main/java/io/netty/channel/ChannelInboundInvoker.java
@@ -15,80 +15,120 @@
  */
 package io.netty.channel;
 
+/**
+ * Channel.ChannelPipeline.ChannelInboundHandlers.next()对应的方法的触发器。
+ */
 public interface ChannelInboundInvoker {
 
     /**
      * A {@link Channel} was registered to its {@link EventLoop}.
      *
+     * 注册Channel到它的EventLoop。
+     * 称从上到下，或从下到上为一个事件循环。
+     *
      * This will result in having the  {@link ChannelInboundHandler#channelRegistered(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * 这会使得当前的Channel的，包含在ChannelPipeline的下一个ChannelInboundHandler的
+     * ChannelInboundHandler#channelRegistered(ChannelHandlerContext)方法被调用
      */
     ChannelInboundInvoker fireChannelRegistered();
 
     /**
      * A {@link Channel} was unregistered from its {@link EventLoop}.
      *
+     * 注销Channel，从它的EventLoop。
+     *
      * This will result in having the  {@link ChannelInboundHandler#channelUnregistered(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * 这会使得当前Channel的ChannelPipeline的下一个ChannelInboundHandler的
+     * ChannelInboundHandler#channelUnregistered(ChannelHandlerContext)方法被调用。
      */
     ChannelInboundInvoker fireChannelUnregistered();
 
     /**
      * A {@link Channel} is active now, which means it is connected.
      *
+     * Channel正在激活，这意味着它被连接。
+     *
      * This will result in having the  {@link ChannelInboundHandler#channelActive(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * 这会导致Channel的ChannelPipeline的下一个ChannelInboundHandler的
+     * ChannelInboundHandler#channelActive(ChannelHandlerContext)方法被调用。
      */
     ChannelInboundInvoker fireChannelActive();
 
     /**
      * A {@link Channel} is inactive now, which means it is closed.
      *
+     * Channel现在被失效，这表示它被关闭。
+     *
      * This will result in having the  {@link ChannelInboundHandler#channelInactive(ChannelHandlerContext)} method
      * called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * 方法Channel -> ChannelPipeline -> next.ChannelInboundHandler -> channelInactive被调用。
+     *
      */
     ChannelInboundInvoker fireChannelInactive();
 
     /**
      * A {@link Channel} received an {@link Throwable} in one of its inbound operations.
      *
+     * Channel收到一个异常，在它的其中一个inbound操作中。
+     *
      * This will result in having the  {@link ChannelInboundHandler#exceptionCaught(ChannelHandlerContext, Throwable)}
      * method  called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * Channel -> ChannelPipeline -> next.ChannelInboundHandler -> ChannelInboundHandler 方法被调用。
      */
     ChannelInboundInvoker fireExceptionCaught(Throwable cause);
 
     /**
      * A {@link Channel} received an user defined event.
      *
+     * Channel收到一个用户定义事件。
+     *
      * This will result in having the  {@link ChannelInboundHandler#userEventTriggered(ChannelHandlerContext, Object)}
      * method  called of the next  {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * Channel -> ChannelPipeline -> next.ChannelInboundHandler -> userEventTriggered 方法被调用。
      */
     ChannelInboundInvoker fireUserEventTriggered(Object event);
 
     /**
      * A {@link Channel} received a message.
      *
+     * Channel收到一个消息。
+     *
      * This will result in having the {@link ChannelInboundHandler#channelRead(ChannelHandlerContext, Object)}
      * method  called of the next {@link ChannelInboundHandler} contained in the  {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * Channel.ChannelPipeline.ChannelInboundHandlers.nextChannelInboundHandler.channelRead 方法被调用。
      */
     ChannelInboundInvoker fireChannelRead(Object msg);
 
     /**
      * Triggers an {@link ChannelInboundHandler#channelReadComplete(ChannelHandlerContext)}
      * event to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
+     *
+     * 触发方法ChannelPipeline.ChannelInboundHandlers.next().channelReadComplete()
      */
     ChannelInboundInvoker fireChannelReadComplete();
 
     /**
      * Triggers an {@link ChannelInboundHandler#channelWritabilityChanged(ChannelHandlerContext)}
      * event to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
+     *
+     * 触发方法ChannelPipeline.ChannelInboundHandlers.next().channelWritabilityChanged()
      */
     ChannelInboundInvoker fireChannelWritabilityChanged();
 }
--- transport/src/main/java/io/netty/channel/ChannelOutboundInvoker.java
+++ transport/src/main/java/io/netty/channel/ChannelOutboundInvoker.java
@@ -21,31 +21,50 @@ import io.netty.util.concurrent.FutureListener;
 import java.net.ConnectException;
 import java.net.SocketAddress;
 
+/**
+ * 参考ChannelInboundInvoker，这应该是Channel.ChannelPipeline.ChannelOutboundHandlers.next().xxx
+ * 方法的触发器。
+ *
+ * 触发器对应的Handler的方法是否会被调用到，取决于Handler是否已经实现了触发器对应的方法。
+ */
 public interface ChannelOutboundInvoker {
 
     /**
      * Request to bind to the given {@link SocketAddress} and notify the {@link ChannelFuture} once the operation
      * completes, either because the operation was successful or because of an error.
+     *
+     * 请求绑定给定的SocketAddress，且通知ChannelFuture一次，当操作完成的时候。要么是绑定操作成功，要么是出错。
+     *
      * <p>
      * This will result in having the
      * {@link ChannelOutboundHandler#bind(ChannelHandlerContext, SocketAddress, ChannelPromise)} method
      * called of the next {@link ChannelOutboundHandler} contained in the {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * Channel.ChannelPipeline.ChannelOutboundHandlers.next().bind方法被调用。
      */
     ChannelFuture bind(SocketAddress localAddress);
 
     /**
      * Request to connect to the given {@link SocketAddress} and notify the {@link ChannelFuture} once the operation
      * completes, either because the operation was successful or because of an error.
+     *
+     * 请求连接给定的SocketAddress，通知ChannelFuture，当操作完成。要么是 操作成功，要么出错。
      * <p>
      * If the connection fails because of a connection timeout, the {@link ChannelFuture} will get failed with
      * a {@link ConnectTimeoutException}. If it fails because of connection refused a {@link ConnectException}
      * will be used.
+     *
+     * 如果连接因超时失败，ChannelFuture将得到ConnectTimeoutException的失败。如果它因为连接被拒绝，将会使用
+     * ConnectException。
+     *
      * <p>
      * This will result in having the
      * {@link ChannelOutboundHandler#connect(ChannelHandlerContext, SocketAddress, SocketAddress, ChannelPromise)}
      * method called of the next {@link ChannelOutboundHandler} contained in the {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * Channel.ChannelPipeline.ChannelOutboundHandlers.next().connect()方法将被触发。
      */
     ChannelFuture connect(SocketAddress remoteAddress);
 
@@ -97,11 +116,16 @@ public interface ChannelOutboundInvoker {
      * Request to deregister from the previous assigned {@link EventExecutor} and notify the
      * {@link ChannelFuture} once the operation completes, either because the operation was successful or because of
      * an error.
+     *
+     * 请求注销，从之前分配的EventExecutor中，通知ChannelFuture一旦操作完成，要么因为操作成功，要么因为出错。
+     *
      * <p>
      * This will result in having the
      * {@link ChannelOutboundHandler#deregister(ChannelHandlerContext, ChannelPromise)}
      * method called of the next {@link ChannelOutboundHandler} contained in the {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * Channel.ChannelPipeline.ChannelOutboundHandlers.next().deregister() 方法被触发。
      *
      */
     ChannelFuture deregister();
@@ -199,11 +223,19 @@ public interface ChannelOutboundInvoker {
      * read, and triggers a
      * {@link ChannelInboundHandler#channelReadComplete(ChannelHandlerContext) channelReadComplete} event so the
      * handler can decide to continue reading.  If there's a pending read operation already, this method does nothing.
+     *
+     * 请求读数据，从Channel到第一个inbound缓冲区，产生一个可被
+     * ChannelInboundHandler#channelRead(ChannelHandlerContext, Object)方法处理的事件，若数据已读，则产生一个可被
+     * ChannelInboundHandler#channelReadComplete(ChannelHandlerContext)方法处理的事件，handler可以决定是否继续读。
+     * 如果准备好了一个待定的读操作，这个方法什么都不做。
+     *
      * <p>
      * This will result in having the
      * {@link ChannelOutboundHandler#read(ChannelHandlerContext)}
      * method called of the next {@link ChannelOutboundHandler} contained in the {@link ChannelPipeline} of the
      * {@link Channel}.
+     *
+     * 这将导致Channel.ChannelPipeline.ChannelOutboundHandlers.next().read方法被调用。
      */
     ChannelOutboundInvoker read();
 
@@ -211,6 +243,9 @@ public interface ChannelOutboundInvoker {
      * Request to write a message via this {@link ChannelHandlerContext} through the {@link ChannelPipeline}.
      * This method will not request to actual flush, so be sure to call {@link #flush()}
      * once you want to request to flush all pending data to the actual transport.
+     *
+     * 请求写一个消息，经由ChannelHandlerContext通过ChannelPipeline。此方法将不会实际刷新，所以确认调用flush()方法，
+     * 一旦你想请求刷新所有待定的数据到真实的数据传输。
      */
     ChannelFuture write(Object msg);
 
@@ -223,6 +258,8 @@ public interface ChannelOutboundInvoker {
 
     /**
      * Request to flush all pending messages via this ChannelOutboundInvoker.
+     *
+     * 请求刷新所有的待定消息，经由当前ChannelOutboundInvoker。
      */
     ChannelOutboundInvoker flush();
 
@@ -238,6 +275,8 @@ public interface ChannelOutboundInvoker {
 
     /**
      * Return a new {@link ChannelPromise}.
+     *
+     * 返回一个ChannelPromise，此关联的ChannelPromise可以作为后续各个请求方法的响应引用。
      */
     ChannelPromise newPromise();
 
@@ -250,6 +289,9 @@ public interface ChannelOutboundInvoker {
      * Create a new {@link ChannelFuture} which is marked as succeeded already. So {@link ChannelFuture#isSuccess()}
      * will return {@code true}. All {@link FutureListener} added to it will be notified directly. Also
      * every call of blocking methods will just return without blocking.
+     *
+     * 创建一个新的ChannelFuture，被标记为已成功。所以ChannelFuture#isSuccess()方法将返回true。所有添加到它的
+     * FutureListener都会被直接通知。并且每一次阻塞调用方法都仅会返回，不会阻塞。
      */
     ChannelFuture newSucceededFuture();
 
@@ -257,22 +299,37 @@ public interface ChannelOutboundInvoker {
      * Create a new {@link ChannelFuture} which is marked as failed already. So {@link ChannelFuture#isSuccess()}
      * will return {@code false}. All {@link FutureListener} added to it will be notified directly. Also
      * every call of blocking methods will just return without blocking.
+     *
+     * 创建一个新的ChannelFuture，它标记为已失败。所以ChannelFuture#isSuccess()将返回false。所有的添加到它的
+     * FutureListener将被直接通知到。并且每一次阻塞调用都将返回，不采用阻塞的方式。
      */
     ChannelFuture newFailedFuture(Throwable cause);
 
     /**
      * Return a special ChannelPromise which can be reused for different operations.
+     *
+     * 返回一个特殊的ChannelPromise，可以被用于不同的操作。
+     *
      * <p>
      * It's only supported to use
      * it for {@link ChannelOutboundInvoker#write(Object, ChannelPromise)}.
+     *
+     * 仅可支持用于ChannelOutboundInvoker#write(Object, ChannelPromise)方法。
+     *
      * </p>
      * <p>
      * Be aware that the returned {@link ChannelPromise} will not support most operations and should only be used
      * if you want to save an object allocation for every write operation. You will not be able to detect if the
      * operation  was complete, only if it failed as the implementation will call
      * {@link ChannelPipeline#fireExceptionCaught(Throwable)} in this case.
+     *
+     * 注意返回的ChannelPromise将不支持大多数操作，也应该仅用于你想保存的分配的对象，给每一次写操作。在这种情况下，你将不能
+     * 检测到ChannelPipeline#fireExceptionCaught(Throwable)。
+     *
      * </p>
      * <strong>Be aware this is an expert feature and should be used with care!</strong>
+     *
+     * 注意这是一个专家的特性，所以要很小心的使用。
      */
     ChannelPromise voidPromise();
 }
