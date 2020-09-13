a8386d267202f65e326411b387c3e63972bb8099
tanshenghui1 <tanshenghui1@yy.com>
2020-09-14 03:12:34 +0800

    HTTP/2测试： 10万次请求，POST averageMicros=447，GET averageMicros=387

--- example/src/main/java/io/netty/example/http2/Http2Consts.java
+++ example/src/main/java/io/netty/example/http2/Http2Consts.java
@@ -2,11 +2,13 @@ package io.netty.example.http2;
 
 public class Http2Consts {
 
-    public static void setSystemProperties() {
+    public static void initSystemProperties() {
         System.setProperty("ssl", "");
         System.setProperty("host", "push-api20.yy.com");
         System.setProperty("port", "443");
         System.setProperty("url", "/get_text");
+//        System.setProperty("url2", "/get_text");
+        System.setProperty("url2data", "{}");
     }
 
 
--- example/src/main/java/io/netty/example/http2/helloworld/client/Http2Client.java
+++ example/src/main/java/io/netty/example/http2/helloworld/client/Http2Client.java
@@ -70,15 +70,15 @@ public final class Http2Client {
     private static Logger logger = LoggerFactory.getLogger(Http2Client.class);
 
     static {
-        Http2Consts.setSystemProperties();
+        Http2Consts.initSystemProperties();
     }
 
     static final boolean SSL = System.getProperty("ssl") != null;
     static final String HOST = System.getProperty("host", "127.0.0.1");
     static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8443" : "8080"));
-    static final String URL = System.getProperty("url", "/whatever");
+    static final String URL = System.getProperty("url");
     static final String URL2 = System.getProperty("url2");
-    static final String URL2DATA = System.getProperty("url2data", "test data!");
+    static final String URL2DATA = System.getProperty("url2data");
 
     public static void main(String[] args) throws Exception {
         new Http2Client().main0();
@@ -91,7 +91,6 @@ public final class Http2Client {
     int streamId = 3;
 
     LinkedBlockingQueue<Long> queue = new LinkedBlockingQueue<Long>();
-    AtomicLong totalCost = new AtomicLong(0L);
     Timer successTimer = Timer.init();
 
     public void main0() throws Exception {
@@ -170,7 +169,7 @@ public final class Http2Client {
                     while (true) {
                         responseHandler.awaitResponses(5, TimeUnit.SECONDS);
                         logger.info("Finished HTTP/2 request(s), streams:{}, totalCost:{}, averageCost:{}, counter:{}, successTimer:{}",
-                                streams, totalCost, totalCost.get() / counter.succeed.get(), counter, successTimer);
+                                streams, counter.length, counter.length.get() / counter.succeed.get(), counter, successTimer);
 
                         try {
                             TimeUnit.SECONDS.sleep(1);
@@ -203,7 +202,7 @@ public final class Http2Client {
             t0.mark();
             successTimer.mark();
             counter.succeed.getAndIncrement();
-            totalCost.getAndAdd(t0.cost());
+            counter.length.getAndAdd(t0.cost());
             queue.offer(1L);
         }
     }
@@ -238,20 +237,31 @@ public final class Http2Client {
         }
         if (URL2 != null) {
             // Create a simple POST request with a body.
-            FullHttpRequest request = new DefaultFullHttpRequest(HTTP_1_1, POST, URL2,
+            FullHttpRequest request = new DefaultFullHttpRequest(HTTP_1_1, POST, getUrl2(),
                     wrappedBuffer(URL2DATA.getBytes(CharsetUtil.UTF_8)));
             request.headers().add(HttpHeaderNames.HOST, hostName);
             request.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(), scheme.name());
             request.headers().add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
             request.headers().add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.DEFLATE);
 
-            responseHandler.put(streamId, channel.write(request), channel.newPromise());
+            ChannelPromise promise = channel.newPromise();
+            promise.addListener(new PromiseListener());
+
+            ChannelFuture future = channel.write(request);
+            future.addListener(new FutureListener());
+
+            responseHandler.put(streamId, future, promise);
+            streamId = (streamId + 2);
         }
         channel.flush();
     }
 
     private String getUrl() {
         return URL + "?=" + System.currentTimeMillis();
+    }
+
+    private String getUrl2() {
+        return URL2 + "?=" + System.currentTimeMillis();
     }
 
 }
